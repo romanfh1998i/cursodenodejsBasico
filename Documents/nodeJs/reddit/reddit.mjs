3af007d2f4b60500df93e89b6be683b97688f31b
unknown <roamfh1998@gmail.com>
2021-09-09 20:30:41 +0200

    Update del reddit -cli y ejemplos basico

--- Documents/nodeJs/reddit/reddit.mjs
+++ Documents/nodeJs/reddit/reddit.mjs
@@ -1,1 +1,22 @@
-console.log('hello from reddit')
+#!/usr/bin/env node
+''
+import fetch from 'node-fetch'
+import open from 'open'
+import yargs from 'yargs'
+
+console.log(process.argv)
+const{argv}=yargs(process.argv)
+const res =await fetch('https://www.reddit.com/.json')
+const data =await res.json()
+const children =data.data.children
+const randomPost=children[Math.floor(Math.random()*children.length)]
+const link=`https://reddit.com${randomPost.data.permalink}`
+if(argv.print){
+    console.log(`
+    title:${randomPost.data.title}
+    link:${link}`)
+}
+else {
+
+    open(link)
+}
